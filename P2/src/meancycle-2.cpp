2c7d764b6ee65ba80cc63a9c8df50bc11ae10f82
David Čadež <david.cadez7@gmail.com>
2023-12-09 11:29:41 +0100

    min mean cycle

--- P2/src/meancycle-2.cpp
+++ P2/src/meancycle-2.cpp
@@ -1,12 +1,11 @@
 #include <deque>
 #include <fstream>  // For reading input files.
 #include <iostream> // For writing to the standard output.
-#include <tuple>
 
 #include "graph.hpp"
 #include "min-join.hpp"
 
-MMC::EdgeWeight weight(MMC::Graph &graph, double gamma) {
+double weight(MMC::Graph &graph, double gamma) {
   MMC::EdgeWeight w = 0;
   for (MMC::EdgeId e{0}; e < graph.num_edges(); e++) {
     w += graph.edge_weight(e) - gamma;
@@ -15,12 +14,10 @@ MMC::EdgeWeight weight(MMC::Graph &graph, double gamma) {
 }
 
 bool is_acyclic(MMC::Graph &graph) {
-  std::vector<MMC::NodeId> seen =
-      std::vector<MMC::NodeId>(graph.num_nodes(), false);
-  std::vector<MMC::NodeId> ancestor =
-      std::vector<MMC::NodeId>(graph.num_nodes());
+  std::vector<MMC::NodeId> seen(graph.num_nodes(), false);
+  std::vector<MMC::NodeId> ancestor(graph.num_nodes());
 
-  std::deque<MMC::NodeId> stack = {MMC::NodeId(0)};
+  std::deque<MMC::NodeId> stack{MMC::NodeId(0)};
   ancestor[0] = 0;
 
   MMC::NodeId v;
@@ -50,51 +47,16 @@ MMC::Graph min_mean_weight_cycle(MMC::Graph &graph) {
   }
 
   double gamma = graph.edge_weight(0);
-  for (MMC::EdgeId e{0}; e < graph.num_edges(); e++) {
-    if (graph.edge_weight(e) > gamma)
-      gamma = graph.edge_weight(e);
-  }
+  for (MMC::EdgeId e{0}; e < graph.num_edges(); e++)
+    gamma = std::max(static_cast<double>(graph.edge_weight(e)), gamma);
+
   MMC::Graph t_join{graph.num_nodes()};
   while (1) {
     t_join = min_join(graph, gamma);
     std::cout << t_join << std::endl;
-    if (weight(t_join, gamma) == 0) {
+    if (weight(t_join, gamma) >= 0)
       return t_join;
-      // MMC::Graph cycle{t_join.num_nodes()};
-
-      // std::vector<MMC::NodeId> seen =
-      //     std::vector<MMC::NodeId>(graph.num_nodes(), false);
-      // std::vector<MMC::NodeId> ancestor =
-      //     std::vector<MMC::NodeId>(graph.num_nodes());
-
-      // std::deque<MMC::NodeId> stack = {MMC::NodeId(0)};
-      // ancestor[0] = 0;
-
-      // MMC::NodeId v;
-      // while (!stack.empty()) {
-      //   v = stack.back();
-      //   stack.pop_back();
-
-      //   MMC::NodeId u;
-      //   for (MMC::EdgeId edge : graph.node(v).outgoing_halfedges()) {
-      //     u = graph.halfedge(edge).target();
-      //     if (!seen[u]) {
-      //       stack.push_back(u);
-      //       ancestor[u] = v;
-      //     } else if (ancestor[v] != u) {
-      //       cycle.add_edge(u, v, t_join.edge_weight(edge));
-      //       while (v != u) {
-      //         cycle.add_edge(v, ancestor[v], graph.edge_weight(edge));
-      //         v = ancestor[v];
-      //       }
-      //       return cycle;
-      //     }
-      //   }
-      //   seen[v] = true;
-      // }
-      // return cycle;
-    }
-    gamma += weight(t_join, gamma) / static_cast<double>(t_join.num_edges());
+    gamma += weight(t_join, gamma) / t_join.num_edges();
   }
 
   MMC::Graph empty = MMC::Graph(graph.num_nodes());
@@ -102,22 +64,18 @@ MMC::Graph min_mean_weight_cycle(MMC::Graph &graph) {
 }
 
 int main(int argc, char **argv) {
-  // If you don't want to write MMC:: all the time,
-  // using a namespace in the implementation of some function is perfectly ok.
-  using namespace MMC;
 
   if (argc != 3) {
     std::cout
         << "Expected exactly two arguments (paths to input and output graph)!"
         << std::endl;
-    return EXIT_FAILURE; // return 1 would do the same, but is way too easy to
-                         // mix up!
+    return EXIT_FAILURE;
   }
 
   std::ifstream input_file_graph{argv[1]};
-  Graph graph = Graph::read_dimacs(input_file_graph);
+  MMC::Graph graph = MMC::Graph::read_dimacs(input_file_graph);
 
-  Graph cycle = min_mean_weight_cycle(graph);
+  MMC::Graph cycle = min_mean_weight_cycle(graph);
 
   std::ofstream output_file_graph{argv[2]};
   output_file_graph << cycle;
--- P2/src/min-join.cpp
+++ P2/src/min-join.cpp
@@ -3,21 +3,21 @@
 std::tuple<std::vector<std::vector<MMC::NodeId>>,
            std::vector<std::vector<double>>>
 all_shortest_paths(MMC::Graph &graph, double gamma) {
-  std::vector<std::vector<MMC::NodeId>> paths = std::vector(
+  std::vector<std::vector<MMC::NodeId>> paths(
       graph.num_nodes(), std::vector<MMC::NodeId>(graph.num_nodes()));
-  std::vector<std::vector<double>> distances =
-      std::vector(graph.num_nodes(),
-                  std::vector<double>(graph.num_nodes(),
-                                      std::numeric_limits<double>::max()));
+  std::vector<std::vector<double>> distances(
+      graph.num_nodes(),
+      std::vector<double>(graph.num_nodes(),
+                          std::numeric_limits<double>::max()));
 
   for (MMC::NodeId v{0}; v < graph.num_nodes(); v++) {
-    auto ancestor = std::vector<MMC::NodeId>(graph.num_nodes());
-    auto distance = std::vector<double>(
-        graph.num_nodes(), std::numeric_limits<MMC::EdgeWeight>::max());
-    distance[v] = 0;
+    std::vector<MMC::NodeId> ancestor(graph.num_nodes());
+    std::vector<double> distance(graph.num_nodes(),
+                                 std::numeric_limits<double>::max());
 
-    std::deque<MMC::NodeId> stack = {v};
+    std::deque<MMC::NodeId> stack{v};
     ancestor[v] = v;
+    distance[v] = 0;
 
     MMC::NodeId u, z;
     double w;
@@ -45,8 +45,8 @@ all_shortest_paths(MMC::Graph &graph, double gamma) {
 
 MMC::Graph min_join(MMC::Graph &graph, double gamma) {
 
-  MMC::NodeId sum = 0;
-  std::vector<MMC::NodeId> conv{}; // convert indices in k_t to nodeid in graph
+  MMC::NodeId num_nodes = 0;
+  std::vector<MMC::NodeId> conv{};
   for (MMC::NodeId u{0}; u < graph.num_nodes(); u++) {
     int count = 0;
     for (MMC::EdgeId e : graph.node(u).outgoing_halfedges()) {
@@ -55,57 +55,43 @@ MMC::Graph min_join(MMC::Graph &graph, double gamma) {
     }
     if (count % 2 == 1) {
       conv.push_back(u);
-      sum++;
+      num_nodes++;
     }
   }
 
   auto [paths, distances] = all_shortest_paths(graph, gamma);
 
-  PerfectMatching solver{static_cast<int>(sum), static_cast<int>(sum)};
-  for (MMC::NodeId u{0}; u < sum; u++)
-    for (MMC::NodeId v{u + 1}; v < sum; v++)
+  PerfectMatching solver{static_cast<int>(num_nodes),
+                         static_cast<int>(num_nodes)};
+  for (MMC::NodeId u{0}; u < num_nodes; u++)
+    for (MMC::NodeId v{u + 1}; v < num_nodes; v++)
       solver.AddEdge(u, v, distances[conv[u]][conv[v]]);
-
-  // for (MMC::EdgeId edge_id{0}; edge_id < k_t.num_edges(); ++edge_id) {
-  //   solver.AddEdge(k_t.halfedge(2 * edge_id + 1).target(),
-  //                  k_t.halfedge(2 * edge_id + 0).target(),
-  //                  k_t.edge_weight(edge_id));
-  // }
 
   solver.Solve();
 
-  std::vector<MMC::EdgeId> t_join{};
-  for (MMC::NodeId u{0}; u < sum; u++) {
-    for (MMC::NodeId v{u + 1}; v < sum; v++) {
-      if (solver.GetSolution(u * sum + v - 1)) {
+  int counter = 0;
+  std::vector<MMC::EdgeId> t_join(graph.num_edges(), 0);
+  for (MMC::NodeId u{0}; u < num_nodes; u++) {
+    for (MMC::NodeId v{u + 1}; v < num_nodes; v++) {
+      if (solver.GetSolution(counter)) {
         std::vector<MMC::EdgeId> cur_path{};
         MMC::NodeId u_g = conv[u];
         MMC::NodeId v_g = conv[v];
         while (u_g != v_g) {
           for (MMC::EdgeId t : graph.node(u_g).outgoing_halfedges())
             if (graph.halfedge(t).target() == paths[u_g][v_g])
-              cur_path.push_back(t / 2);
-
+              t_join[t / 2] = t_join[t / 2] ? 0 : 1;
           u_g = paths[u_g][v_g];
         }
-        std::vector<MMC::EdgeId> sym_dif;
-        std::sort(t_join.begin(), t_join.end());
-        std::sort(cur_path.begin(), cur_path.end());
-        std::set_symmetric_difference(t_join.begin(), t_join.end(),
-                                      cur_path.begin(), cur_path.end(),
-                                      std::back_inserter(sym_dif));
-        t_join = sym_dif;
       }
+      counter++;
     }
   }
 
   MMC::Graph solution{graph.num_nodes()};
   for (MMC::NodeId v{0}; v < graph.num_nodes(); v++) {
     for (MMC::EdgeId e : graph.node(v).outgoing_halfedges()) {
-      if (((std::find(t_join.begin(), t_join.end(), e / 2) != t_join.end()) &&
-           graph.halfedge_weight(e) >= gamma) ||
-          ((std::find(t_join.begin(), t_join.end(), e / 2) == t_join.end()) &&
-           graph.halfedge_weight(e) < gamma))
+      if (t_join[e / 2] != (graph.halfedge_weight(e) < gamma))
         solution.add_edge(v, graph.halfedge(e).target(),
                           graph.halfedge_weight(e));
     }
